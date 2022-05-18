1040ebc31e8963c89f7069767220ea2618a13353
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-05-18 19:37:10 -0300

    Restauración DB, modelo de usuarios
    (Continuar por 369)

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import connectDB from "./config/db"
 import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes"
+
 const app = express()
 dotenv.config()
 connectDB()
 
+app.use('/api/users', userRoutes)
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
